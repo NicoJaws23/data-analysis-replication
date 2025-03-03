5073c48d9b727668ef438e975c6fa8ed6890b9cf
NicoJaws23 <nico.jaworski@utexas.edu>
2025-03-03 12:18:42 -0600

    second commit

--- dataAnalysisReplicationCode.R
+++ dataAnalysisReplicationCode.R
@@ -1,1 +1,7 @@
 #Codeeeeeee
+library(brms)
+library(tidyverse)
+f1 <- "https://raw.githubusercontent.com/NicoJaws23/data-analysis-replication/refs/heads/main/DataForModelSelection.csv"
+df <- read_csv(f1)
+f2 <- "https://raw.githubusercontent.com/NicoJaws23/data-analysis-replication/refs/heads/main/DataForPredAndIntergroup.csv"
+pred <- read_csv(f2)
